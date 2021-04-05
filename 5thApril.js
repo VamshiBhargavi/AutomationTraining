617bd46f55a8a833be3888dcd8ea76f1f9ca73be
VamshiBhargavi <bhargavi.vamshi@yahoo.com>
2021-04-05 11:08:47 +0000

    Training1

---
+++ 5thApril.js
@@ -0,0 +1,13 @@
+"use strict"
+console.log("hello");
+
+let x=10;
+let y=20;
+let z=x+y;
+
+console.log(z);
+
+let employee = ["name", "id", "salary"];
+console.log(employee);
+employee.push("project", "team");
+console.log(employee);
