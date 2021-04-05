cbe01cf10fed68d761130489bb7320e9e67e8a7b
VamshiBhargavi <bhargavi.vamshi@yahoo.com>
2021-04-05 11:25:27 +0000

    New file

---
+++ Conditions.js
@@ -0,0 +1,18 @@
+n=10
+console.log(n%2);
+
+//% = modulus
+// == : compare data
+// === : compare data and type
+
+
+console.log(n%2==1);
+
+if(n%2==0){
+    console.log("Even Number");
+    }
+    else{
+        console.log("Odd number");
+        
+    }
+
